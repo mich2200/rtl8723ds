75b11e99be6cc02cea6a00310633b191de962098
mich2200 <77480226+mich2200@users.noreply.github.com>
2024-04-16 21:50:28 +0200

    Update osdep_service_linux.h

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -58,7 +58,6 @@
 #include <linux/list.h>
 #include <linux/vmalloc.h>
 #include <linux/signal.h>
-#include <linux/sched/signal.h>
 
 #if (LINUX_VERSION_CODE <= KERNEL_VERSION(2, 5, 41))
 	#include <linux/tqueue.h>
